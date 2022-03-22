ab5c1f79b566a372f66e707a1716cb715026d175
Shrusti <shrustiparmar1411@gmail.com>
2022-03-22 15:02:08 +0530

    added environment variable

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { UserListCardWrapper } from "./components/UserListCardWrapper/UserListCardWrapper";
+
+function App() {
+  return (
+    <>
+      <UserListCardWrapper />
+    </>
+  );
+}
+
+export default App;
